cfeb65a94eeee52de1027b1086067f329a11f311
xpeng <1250807313@qq.com>
2025-05-02 16:36:53 +0800

    增加发送私信功能

--- src/main/java/com/newcoder/community/dao/MessageMapper.java
+++ src/main/java/com/newcoder/community/dao/MessageMapper.java
@@ -31,4 +31,10 @@ public interface MessageMapper {
 
     // 查询未读私信的数量
     int selectLetterUnreadCount(int userId, String conversationId);
+
+    // 新增消息
+    int insertMessage(Message message);
+
+    // 修改消息的状态
+    int updateMessage(List<Integer> ids, int status);
 }
--- src/main/java/com/newcoder/community/service/MessageService.java
+++ src/main/java/com/newcoder/community/service/MessageService.java
@@ -2,8 +2,10 @@ package com.newcoder.community.service;
 
 import com.newcoder.community.dao.MessageMapper;
 import com.newcoder.community.entity.Message;
+import com.newcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -21,6 +23,9 @@ public class MessageService {
 
     @Autowired
     private MessageMapper messageMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
 
     public List<Message> findConversations(int userId, int offset, int limit) {
         return messageMapper.selectConversations(userId, offset, limit);
@@ -40,5 +45,15 @@ public class MessageService {
 
     public int findLetterUnreadCount(int userId, String conversationId) {
         return messageMapper.selectLetterUnreadCount(userId, conversationId);
+    }
+
+    public int addMessage(Message message) {
+        message.setContent(HtmlUtils.htmlEscape(message.getContent()));
+        message.setContent(sensitiveFilter.filter(message.getContent()));
+        return messageMapper.insertMessage(message);
+    }
+
+    public int readMessage(List<Integer> ids) {
+        return messageMapper.updateMessage(ids,1);
     }
 }
