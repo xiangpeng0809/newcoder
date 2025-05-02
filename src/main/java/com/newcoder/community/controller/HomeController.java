6c59913398f021131712c5d8190073dcd383fc25
xpeng <1250807313@qq.com>
2025-05-02 17:04:23 +0800

    增加统一处理异常功能

--- src/main/java/com/newcoder/community/controller/HomeController.java
+++ src/main/java/com/newcoder/community/controller/HomeController.java
@@ -56,4 +56,8 @@ public class HomeController {
         return "/index";
     }
 
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
+    }
 }
---
+++ src/main/java/com/newcoder/community/controller/advice/ExceptionAdvice.java
@@ -0,0 +1,46 @@
+package com.newcoder.community.controller.advice;
+
+import com.newcoder.community.util.CommunityUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * ClassName: ExceptionAdvice
+ * Package: com.newcoder.community.controller.advice
+ * Description:
+ *
+ * @author 向鹏
+ * @version 1.0
+ * @create 2025/5/2 16:51
+ */
+@ControllerAdvice(annotations = Controller.class)
+public class ExceptionAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    @ExceptionHandler({Exception.class})
+    public void handleException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.error("服务器发生异常：" + e.getMessage());
+        for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+            logger.error(stackTraceElement.toString());
+        }
+
+        String xRequestedWith = request.getHeader("x-requested-with");
+        if ("XMLHttpRequest".equals(xRequestedWith)) {
+            response.setContentType("application/plain;charset=utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.write(CommunityUtil.getJSONString(1,"服务器异常!"));
+        } else {
+            response.sendRedirect(request.getContextPath() + "/error");
+        }
+    }
+
+}
