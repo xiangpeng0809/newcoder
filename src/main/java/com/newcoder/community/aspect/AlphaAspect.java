b63898edbd12cb55a4ffb8fdcb7dce45c3c26288
xpeng <1250807313@qq.com>
2025-05-02 17:31:24 +0800

    aop示例

---
+++ src/main/java/com/newcoder/community/aspect/AlphaAspect.java
@@ -0,0 +1,52 @@
+package com.newcoder.community.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+/**
+ * ClassName: AlphaAspect
+ * Package: com.newcoder.community.aspect
+ * Description:
+ *
+ * @author 向鹏
+ * @version 1.0
+ * @create 2025/5/2 17:11
+ */
+//@Component
+//@Aspect
+public class AlphaAspect {
+
+    @Pointcut("execution(* com.newcoder.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before() {
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after() {
+        System.out.println("after");
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterReturning() {
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("around before");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+}
