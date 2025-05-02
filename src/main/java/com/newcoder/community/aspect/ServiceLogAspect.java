97fe72805da7642a0f2704726208349a9154bec9
xpeng <1250807313@qq.com>
2025-05-02 17:31:09 +0800

    增加统一记录日志功能

---
+++ src/main/java/com/newcoder/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,48 @@
+package com.newcoder.community.aspect;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * ClassName: ServiceLogAspect
+ * Package: com.newcoder.community.aspect
+ * Description:
+ *
+ * @author 向鹏
+ * @version 1.0
+ * @create 2025/5/2 17:22
+ */
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* com.newcoder.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+        // 用户 xx 在 xx 访问了 com.newcoder.community.service.xxx
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s],在[%s],访问了[%s].", ip, now, target));
+    }
+
+}
