51655fa6c02d272e711f925a8ed9f85d245edcc2
xpeng <1250807313@qq.com>
2025-05-03 21:19:53 +0800

    增加发送系统通知功能

---
+++ src/main/java/com/newcoder/community/event/EventConsumer.java
@@ -0,0 +1,71 @@
+package com.newcoder.community.event;
+
+import com.alibaba.fastjson.JSONObject;
+import com.newcoder.community.entity.Event;
+import com.newcoder.community.entity.Message;
+import com.newcoder.community.service.MessageService;
+import com.newcoder.community.util.CommunityConstant;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ClassName: EventConsumer
+ * Package: com.newcoder.community.event
+ * Description:
+ *
+ * @author 向鹏
+ * @version 1.0
+ * @create 2025/5/3 20:31
+ */
+@Component
+public class EventConsumer implements CommunityConstant {
+
+    @Autowired
+    private static final Logger logger = LoggerFactory.getLogger(EventConsumer.class);
+
+    @Autowired
+    private MessageService messageService;
+
+    @KafkaListener(topics = {TOPIC_COMMENT, TOPIC_LIKE, TOPIC_FOLLOW})
+    public void handleCommentMessage(ConsumerRecord record) {
+        if (record == null || record.value() == null) {
+            logger.error("消息内容为空");
+            return;
+        }
+
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误");
+            return;
+        }
+
+        // 发送站内通知
+        Message message = new Message();
+        message.setFromId(SYSTEM_USER_ID);
+        message.setToId(event.getEntityId());
+        message.setConversationId(event.getTopic());
+        message.setCreateTime(new Date());
+
+        Map<String, Object> content = new HashMap<>();
+        content.put("userId", event.getUserId());
+        content.put("entityType",event.getEntityType());
+        content.put("entityId",event.getEntityId());
+
+        if (event.getData().isEmpty()) {
+            for (Map.Entry<String, Object> entry : event.getData().entrySet()) {
+                content.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        message.setContent(JSONObject.toJSONString(content));
+        messageService.addMessage(message);
+    }
+}
---
+++ src/main/java/com/newcoder/community/event/EventProducer.java
@@ -0,0 +1,31 @@
+package com.newcoder.community.event;
+
+import com.alibaba.fastjson.JSONObject;
+import com.newcoder.community.entity.Event;
+import io.lettuce.core.json.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * ClassName: EventProducer
+ * Package: com.newcoder.community.event
+ * Description:
+ *
+ * @author 向鹏
+ * @version 1.0
+ * @create 2025/5/3 20:28
+ */
+@Component
+public class EventProducer {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    // 处理事件
+    public void fireEvent(Event event) {
+        // 将事件发布到指定的主题
+        kafkaTemplate.send(event.getTopic(), JSONObject.toJSONString(event));
+    }
+
+}
